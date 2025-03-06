5c21fc1c12a7d4370cffc7404e59d5e43faa076b
ArvydasG9 <117637633+ArvydasG9@users.noreply.github.com>
2025-03-06 08:06:40 +0200

    after lesson 2

---
+++ examples/openai-client/openai_client_demo.py
@@ -0,0 +1,24 @@
+import os
+from openai import OpenAI
+
+from dotenv import load_dotenv
+from rich import print
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Generate me a python code to read a file",
+        }
+    ],
+    model="gpt-4o",
+    # model="gpt-4o-mini",
+)
+
+print(chat_completion.choices[0].message.content)
